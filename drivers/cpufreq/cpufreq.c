26ce34143a12e0fb7d1098a738404163c415d549
Debayan Kar <kardebayan3@gmail.com>
2024-04-03 01:14:33 +0530

    drivers/cpufreq: import realme changes
    Signed-off-by: Debayan Kar <kardebayan3@gmail.com>

--- drivers/cpufreq/cpufreq.c
+++ drivers/cpufreq/cpufreq.c
@@ -218,6 +218,14 @@ unsigned int cpufreq_generic_get(unsigned int cpu)
 }
 EXPORT_SYMBOL_GPL(cpufreq_generic_get);
 
+#ifdef CONFIG_MACH_REALME_TRINKET
+struct list_head *get_cpufreq_policy_list(void)
+{
+    return &cpufreq_policy_list;
+}
+EXPORT_SYMBOL(get_cpufreq_policy_list);
+#endif /* CONFIG_MACH_REALME_TRINKET */
+
 /**
  * cpufreq_cpu_get: returns policy for a cpu and marks it busy.
  *
