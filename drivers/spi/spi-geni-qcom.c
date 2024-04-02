8f9cdc66f885893ab14c5b2509ff92d69f265633
Debayan Kar <kardebayan3@gmail.com>
2024-04-03 01:15:34 +0530

    drivers/spi: import realme changes
    Signed-off-by: Debayan Kar <kardebayan3@gmail.com>

--- drivers/spi/spi-geni-qcom.c
+++ drivers/spi/spi-geni-qcom.c
@@ -1472,6 +1472,14 @@ static void geni_spi_handle_tx(struct spi_geni_master *mas)
 
 	if (!mas->cur_xfer)
 		return;
+#ifdef CONFIG_MACH_REALME_TRINKET
+//Lijie.Yang@ODM_WT.BSP.Kernel.Stability.1941873, 2020/06/08, modify for debug tx buff is none issue
+	if (mas->cur_xfer->tx_buf == NULL) {
+		dev_err(mas->dev, "geni_spi_handle_tx:[%s] tx_buf is Null len = %d tx_rem_bytes = %d\n",
+			mas->cur_xfer->len,mas->tx_rem_bytes);
+		return;
+	}
+#endif
 
 	/*
 	 * For non-byte aligned bits-per-word values:
@@ -1531,6 +1539,15 @@ static void geni_spi_handle_rx(struct spi_geni_master *mas)
 
 	if (!mas->cur_xfer)
 		return;
+
+#ifdef CONFIG_MACH_REALME_TRINKET 
+//Lijie.Yang@ODM_WT.BSP.Kernel.Stability.1941873, 2020/06/08, modify for debug tx buff is none issue
+	if (mas->cur_xfer->rx_buf == NULL) {
+		dev_err(mas->dev, "geni_spi_handle_rx:[%s] rx_buf is Null len = %d rx_rem_bytes = %d\n",
+			mas->cur_xfer->len,mas->rx_rem_bytes);
+		return;
+	}
+#endif
 
 	rx_fifo_status = geni_read_reg(mas->base, SE_GENI_RX_FIFO_STATUS);
 	rx_buf = mas->cur_xfer->rx_buf;
@@ -1951,6 +1968,7 @@ static int spi_geni_suspend(struct device *dev)
 {
 	int ret = 0;
 
+	#if 0
 	if (!pm_runtime_status_suspended(dev)) {
 		struct spi_master *spi = get_spi_master(dev);
 		struct spi_geni_master *geni_mas = spi_master_get_devdata(spi);
@@ -1958,7 +1976,7 @@ static int spi_geni_suspend(struct device *dev)
 		if (list_empty(&spi->queue) && !spi->cur_msg) {
 			GENI_SE_ERR(geni_mas->ipc, true, dev,
 					"%s: Force suspend", __func__);
-			ret = spi_geni_runtime_suspend(dev);
+			/*ret = spi_geni_runtime_suspend(dev);
 			if (ret) {
 				GENI_SE_ERR(geni_mas->ipc, true, dev,
 					"Force suspend Failed:%d", ret);
@@ -1966,11 +1984,13 @@ static int spi_geni_suspend(struct device *dev)
 				pm_runtime_disable(dev);
 				pm_runtime_set_suspended(dev);
 				pm_runtime_enable(dev);
-			}
+			}*/
 		} else {
 			ret = -EBUSY;
 		}
 	}
+	#endif
+	printk("come in spi_geni_suspend %d\n",ret);
 	return ret;
 }
 #else
